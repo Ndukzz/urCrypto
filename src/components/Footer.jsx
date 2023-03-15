7036d62eaeda4458d78187318c0d12b18285a0a9
ndukz <ebukaemannuel@gmail.com>
2023-03-15 05:38:33 +0100

    Project Completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,78 @@
+const Footer = () => {
+  return (
+    <div className='mx-10 my-16 sm:flex sm:w-full sm:mx-0'>
+      <div className="sm:mx-10">
+        <div className='footer-logo flex justify-center items-center'>
+          <img className="h-20 sm:h-10" src="./src/images/logo.png" alt="logo" />
+          <h1 className="default px-2 font-bold text-5xl sm:text-2xl"><span>ur</span>Crypto&#8226;</h1>
+        </div>
+        <p className="text-center opacity-80 font-medium text-2xl py-2 sm:text-sm">Take your crypto to the next level</p>
+        <ul className="w-3/5 flex justify-around mx-auto sm:justify-around">
+          <li><img src="./src/images/facebook.png" alt="" /></li>
+          <li><img src="./src/images/twitter.png" alt="" /></li>
+          <li><img src="./src/images/instagram.png" alt="" /></li>
+          <li><img src="./src/images/linkedin.png" alt="" /></li>
+          <li><img src="./src/images/youtube.png" alt="" /></li>
+        </ul>
+      </div>
+
+      <section className="flex flex-wrap mt-8 sm:w-full sm:mt-0 sm:flex-wrap lg:justify-between sm:mr-10">
+        <div className="my-5 w-1/2 sm:w-32">
+          <h3 className="title">Company</h3>
+          <ul>
+            <li className="content-text py-2  ">About us</li>
+            <li className="content-text py-2">Blog</li>
+            <li className="content-text py-2">Careers</li>
+            <li className="content-text py-2">Student</li>
+            <li className="content-text py-2">Security</li>
+            <li className="content-text py-2">Trust and Safety</li>
+            <li className="content-text py-2">Newsroom</li>
+            <li className="content-text py-2">Videos</li>
+          </ul>
+        </div>
+        <div className="my-5 w-1/2 sm:w-32">
+          <h3 className="title ">Learn</h3>
+          <ul>
+            <li className="content-text py-2">What's Trending</li>
+            <li className="content-text py-2">Product News</li>
+            <li className="content-text py-2">Events</li>
+            <li className="content-text py-2">University</li>
+            <li className="content-text py-2">Research</li>
+            <li className="content-text py-2">Market Updates</li>
+          </ul>
+        </div>
+         <div className="my-5 w-1/2 sm:w-32">
+          <h3 className="title">Products</h3>
+          <ul>
+            <li className="content-text py-2">Stock & Fund</li>
+            <li className="content-text py-2">Cash Card</li>
+            <li className="content-text py-2">Crypto</li>
+            <li className="content-text py-2">Options</li>
+            <li className="content-text py-2">Gold</li>
+            <li className="content-text py-2">Learn Snacks</li>
+          </ul>
+        </div>
+        <div className="my-5 w-1/2 sm:w-32">
+          <h3 className="title">Support</h3>
+          <ul>
+            <li className="content-text py-2">Support Center</li>
+            <li className="content-text py-2">Contact Us</li>
+            <li className="content-text py-2">System Status</li>
+            <li className="content-text py-2">Areas of Availability </li>
+          </ul>
+        </div>
+        <div className="my-5 w-1/2 sm:w-32">
+          <h3 className="title">Resources</h3>
+          <ul>
+            <li className="content-text py-2">Prices</li>
+            <li className="content-text py-2">Site Widgets</li>
+            <li className="content-text py-2">Tax </li>
+            <li className="content-text py-2">Support</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+const NavBar = () => {
+  const [ menu, setMenu ] = useState(false)
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+    console.log(menu);
+  }
+
+  return (
+    <nav>
+      <div className='m-10 flex justify-between justify-items-center items-center h-9 text-base '>
+        <div className="flex items-center text-xl font-bold">
+          <img className="h-10" src="./src/images/logo.png" alt="logo" />
+          <h1 className="default px-2"><span>ur</span>Crypto&#8226;</h1>
+        </div>
+        <ul className={`hidden lg:flex justify-around w-2/5`}>
+          <li>Products</li>
+          <li>Price</li>
+          <li>Learn</li>
+          <li>Support</li>
+        </ul>
+        <div className={`hidden sm:flex`}>
+          <button className="px-5">Sign In</button>
+          <button className='border-button'>Get Started</button>
+        </div>
+        <img className="h-10 lg:hidden" onClick={toggleMenu} src="./src/images/menu.png" alt="menu" />
+      </div>
+      {menu &&
+      <div className="flex justify-center flex-col items-center lg:hidden">
+        <ul className={`text-center sm:block justify-around w-2/5 lg:${!menu && "hidden"}`}>
+          <li className="pb-2">Products</li>
+          <li className="p-2">Price</li>
+          <li className="p-2">Learn</li>
+          <li className="p-2">Support</li>
+        </ul> 
+
+        <div className={`flex flex-col sm:hidden`}>
+          <button className="p-2">Sign In</button>
+          <button className='border-button'>Get Started</button>
+        </div>
+      </div>
+      }
+    </nav>
+  )
+}
+
+export default NavBar
